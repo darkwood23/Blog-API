2bd32bf7a46bacee6cc6d1cbdf3f7d1f4a919df5
Dark Wood <anonymoushackerintheweb@gmail.com>
2024-03-03 11:17:52 +0545

    Fixed bugs in userController again

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,13 @@
 const User = require("../models/user")
 const asyncHandler = require("express-async-handler")
 const jwt = require("jsonwebtoken")
-const { genPassword } = require("../lib/passportUtils")
+const { genPassword, checkPassword } = require("../lib/passportUtils")
 require("dotenv").config()
 
 module.exports.create_user = asyncHandler ( async (req, res, next) => {
     const user = new User({
-        username: "Hackerman",
-        password: genPassword(process.env.USER_PASSWORD)
+        username: req.headers['username'],
+        password: genPassword(req.headers['password'])
     })
 
     const userExists = await User.findOne({ username: user.username })
@@ -26,23 +26,37 @@ module.exports.create_user = asyncHandler ( async (req, res, next) => {
 // module.exports.log_in_post = passport.authenticate('local')
 module.exports.log_in_post = asyncHandler ( async (req, res, next) => {
     const user = {
-        username: req.headers['Username'],
-        password: req.headers['Password']
+        username: req.headers['username'],
+        password: req.headers['password']
     }
-    jwt.sign({user}, 'secretkey', { expiresIn: '12h' }, ( err, token ) => {
-        if(!err) {
-            res.json({
-                message: 'User logged in successfully',
-                token,
-                user,
-                status: 200
+    
+    const userExists = await User.findOne({ username: user.username })
+    
+    if (userExists) {
+        const correctPassword = checkPassword(user.password, userExists.password)
+
+        if (correctPassword) {
+            jwt.sign({user}, 'secretkey', { expiresIn: '12h' }, ( err, token ) => {
+                if(!err) {
+                    res.json({
+                        message: 'User logged in successfully',
+                        token,
+                        user,
+                        status: 200
+                    })
+                } else {
+                    res.json({
+                        message: 'Error logging user',
+                        error: err
+                    })
+                    return
+                }
             })
         } else {
-            res.json({
-                message: 'Error logging user',
-                error: err
-            })
-            return
+            res.sendStatus(401)
         }
-    })
+    } else {
+        res.sendStatus(404)
+    }
+
 })
